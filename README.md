d8fba91f019caa105f9e57e7f140d4740800e814
Glitch (html-5-boilerplate) <none>
2019-04-23 20:29:24 +0000

    ⚾️🎽 Checkpoint ./README.md:195043/728 ./public/index.html:195043/7889 ./public/css/main.css:195043/16102 ./public/js/main.js:195043/2760 ./public/404.html:195043/169

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-# HTML5 Boilerplate
-A front-end template for building fast, robust, and adaptable web apps or sites.
+# Running Pace Calculator
 
 ## It provides:
-- A lean, mobile-friendly HTML template; optimized Google Analytics snippet; placeholder touch-device icon; and docs covering dozens of extra tips and tricks.
-
-- Normalize.css and helpers
-Includes Normalize.css — a modern, HTML5-ready alternative to CSS resets — and further base styles, helpers, media queries, and print styles.
-
-- jQuery and Modernizr
-The latest minified versions of two best-of-breed libraries: jQuery (via Google’s CDN, with local fallback) and the Modernizr feature detection library.
+- Learn what will be the pace you need to keep in order to make a new PR in a marathon (or other distance)
---
+++ public/js/main.js
@@ -0,0 +1,109 @@
+calcPace();
+
+loadEventListeners();
+
+//
+//
+//
+function loadEventListeners(){
+  document.getElementById('hourSlide').addEventListener('input', calcPace);
+  document.getElementById('minuteSlide').addEventListener('input', calcPace);
+  document.getElementById('secondSlide').addEventListener('input', calcPace);
+  document.getElementById('distanceRadioFields').addEventListener('click', calcPace);
+}
+
+//
+//
+//
+function calcPace() {
+  function getRaceDistance() {
+    let radios = document.getElementsByName('distance');
+    let val;
+    for (var i = 0; i < radios.length; i++) {
+      if (radios[i].type === 'radio' && radios[i].checked) {
+          // get value, set checked flag or do whatever you need to
+          val = radios[i].value;
+        }
+      }
+    return val; // return value of checked radio or undefined if none checked
+    }
+  
+//
+//
+//
+function getHours() {
+	let myValue = document.getElementById('hourSlide').value;
+  document.getElementById('hourSliderValue').innerHTML = myValue;
+  return parseFloat(myValue*60);
+}
+
+//
+//
+//
+function getMinutes() {
+	let myValue = document.getElementById('minuteSlide').value;
+  if(myValue >= 10){
+      document.getElementById('minuteSliderValue').innerHTML = myValue;
+    } else if(myValue <= 9) {
+      document.getElementById('minuteSliderValue').innerHTML = '0' + myValue;
+    }
+  return parseFloat(myValue);
+}
+
+//
+//
+//  
+function getSeconds() {
+	let myValue = document.getElementById('secondSlide').value;
+  if(myValue >= 10){
+      document.getElementById('secondSliderValue').innerHTML = myValue;
+    } else if(myValue <= 9) {
+      document.getElementById('secondSliderValue').innerHTML = '0' + myValue;
+    }
+  return parseFloat(myValue / 60);
+}
+
+//
+//
+//
+let totalMinutes = getHours() + getMinutes() + getSeconds();
+let justSeconds = Math.round(((totalMinutes % getRaceDistance())/getRaceDistance())*60);
+let wholeMinute = Math.round((totalMinutes / getRaceDistance()) - ((totalMinutes % getRaceDistance())/getRaceDistance()));
+  
+//
+//
+//
+function justSecondsFormatted() {
+  if (justSeconds > 9 ){
+    return justSeconds;
+  }else if (justSeconds < 10 ) {
+    return '0' + justSeconds;
+  }
+};
+  
+//
+//
+//
+function finalPace(){
+  if (justSecondsFormatted() == 60) {
+    let setPace = wholeMinute + 1 + ':00';
+    return setPace;
+  }else {
+    let setPace = wholeMinute + ':' + justSecondsFormatted();
+    return setPace;
+  }
+}
+ 
+//
+// convert min/mile to min/km --> 0.62137119223733
+//
+function finalPaceKm(){
+ let pace = justSeconds;
+  console.log("pace: "+pace);
+ pace = pace * 0.62137119223733
+ return pace; 
+}
+  
+document.getElementById('PaceValue').innerHTML = finalPace() + '<span> Min/Mi</span>';
+document.getElementById('PaceValue-km').innerHTML = finalPaceKm() + '<span> Min/Km</span>';
+}
